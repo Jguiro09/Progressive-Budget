c073b21aec804dab318bd89b5cfc236629d6365d
Jacob Guiro <jguiro09@gmail.com>
2021-08-23 12:18:59 -0500

    checkDatabase function created/completed

--- public/db.js
+++ public/db.js
@@ -22,3 +22,38 @@ request.onerror = function (e) {
     console.log(e.target.errorCode)
 };
 
+// Inputting any info from the database into API (if theres any)
+function checkDatabase () {
+    // Creates a transaction for our database (A transaction is a method within IDBDatabase that will return a "Transaction" object with our objectstore within it.)
+    let transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+    // Accessing our "BudgetStore" object specifically within our transaction
+    const store = transaction.objectStore('BudgetStore');
+
+    // Gets all the records within our 'BudgetStore' so we can store it into our API
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'applcation/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((response) => response.json())
+            .then((res) => {
+                if (res.length !== 0) {
+                    transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+                    const currentStore = transaction.objectStore('BudgetStore');
+
+                    currentStore.clear();
+                    console.log('Clearing!');
+                }
+            })
+        }
+    }
+}
