13b1d1ba85f54dc5c453581f42b646cfd41a2e68
Jacob Guiro <jguiro09@gmail.com>
2021-08-23 12:59:07 -0500

    Service-worker created, Variables created

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/db.js',
+    '/index.html',
+    '/index.js',
+    '/styles.css',
+    '/manifest.json'
+];
+
+const CACHE_NAME = 'static-cache-v3';
+const DATA_CACHE_NAME = 'data-cache-v1';
