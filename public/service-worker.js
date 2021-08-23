fa858d8d6379321c54d9a0681db48ed9b3d969d3
Jacob Guiro <jguiro09@gmail.com>
2021-08-23 13:02:47 -0500

    created eventListener for install

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,3 +9,14 @@ const FILES_TO_CACHE = [
 
 const CACHE_NAME = 'static-cache-v3';
 const DATA_CACHE_NAME = 'data-cache-v1';
+
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Your files have been pre-cached!');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWainting();
+})
