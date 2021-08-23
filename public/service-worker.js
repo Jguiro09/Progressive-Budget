02fb85d84edf27763e442cf2aacec73e2959dcf4
Jacob Guiro <jguiro09@gmail.com>
2021-08-23 13:06:35 -0500

    created eventlistener for activate

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,8 +10,8 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = 'static-cache-v3';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
-self.addEventListener("install", function(evt) {
-    evt.waitUntil(
+self.addEventListener("install", function(e) {
+    e.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             console.log('Your files have been pre-cached!');
             return cache.addAll(FILES_TO_CACHE);
@@ -19,4 +19,21 @@ self.addEventListener("install", function(evt) {
     );
 
     self.skipWainting();
+})
+
+self.addEventListener("activate", function(e) {
+    e.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("removing old", key)
+                        return caches.delete(key);
+                    }
+                })
+            )
+        })
+    );
+
+    self.clients.claim();
 })
