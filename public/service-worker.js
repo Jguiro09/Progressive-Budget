bee655ff15f7360ee2c7dbf6d59e859c155ef559
Jacob Guiro <jguiro09@gmail.com>
2021-08-24 10:44:06 -0500

    reformmated service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,65 +7,52 @@ const FILES_TO_CACHE = [
     '/manifest.webmanifest'
 ];
 
-const CACHE_NAME = 'static-cache-v1';
-const DATA_CACHE_NAME = 'data-cache-v1';
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
 
 self.addEventListener("install", function(e) {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
+        caches.open(PRECACHE).then(cache => {
             console.log('Your files have been pre-cached!');
             return cache.addAll(FILES_TO_CACHE);
         })
+        .then(self.skipWaiting())
     );
-
-    self.skipWaiting();
 })
 
 self.addEventListener("activate", function(e) {
+    const currentCaches = [PRECACHE, RUNTIME]
     e.waitUntil(
         caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("removing old", key)
-                        return caches.delete(key);
-                    }
-                })
-            )
+            return keyList.filter((key) => !currentCaches.includes(key));
         })
+        .then((cachesToDelete) => {
+            return Promise.all(
+                cachesToDelte.map((cacheToDelete) => {
+                    return caches.delete(cacheToDelete);
+                })
+            );
+        })
+        .then(() => self.clients.claim())
     );
-
-    self.clients.claim();
 })
 
 self.addEventListener('fetch', function(e) {
-    if(e.request.url.includes('/api/')) {
+    if(e.request.url.startsWith(self.location.origin)) {
         e.respondWith(
-            caches.open(DATA_CACHE_NAME).then(cache => {
-                return fetch(e.request)
-                    .then (response => {
-                        if(response.status === 200) {
-                            cache.put(e.request.url, response.clone());
-                        }
+            caches.match(e.request).then((cachedResponse) => {
+                if(cachedResponse){
+                    return cachedResponse;
+                }
 
-                        return response;
+                return caches.open(RUNTIME).then((cache) => {
+                    return fetch(e.request).then((response) => {
+                        return cache.put(e.request, response.clone()).then(()=> {
+                            return response;
+                        })
                     })
-                    .catch(err => {
-                        return cache.match(e.request)
-                    })
-            }).catch(err => {
-                console.log(err);
+                })
             })
         );
-
-        return;
     }
-
-    e.respondWith(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.match(evt.request).then(response => {
-                return response || fetch.apply(e.request);
-            })
-        })
-    )
 })
