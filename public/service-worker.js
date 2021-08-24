591a847128533befc990c180f12dd97aee3f0bf7
Jacob Guiro <jguiro09@gmail.com>
2021-08-24 11:15:07 -0500

    reformatted service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,20 @@
 const FILES_TO_CACHE = [
-    '/',
-    '/db.js',
-    '/index.html',
-    '/index.js',
-    '/styles.css',
-    '/manifest.webmanifest'
+    "/",
+    "index.html",
+    "styles.css",
+    "index.js",
+    "db.js",
+    "manifest.json",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
 ];
 
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const CACHE_NAME = 'precache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 self.addEventListener("install", function(e) {
     e.waitUntil(
-        caches.open(PRECACHE).then(cache => {
+        caches.open(CACHE_NAME).then(cache => {
             console.log('Your files have been pre-cached!');
             return cache.addAll(FILES_TO_CACHE);
         })
@@ -21,10 +23,10 @@ self.addEventListener("install", function(e) {
 })
 
 self.addEventListener("activate", function(e) {
-    const currentCaches = [PRECACHE, RUNTIME]
+    const currentCaches = [CACHE_NAME, DATA_CACHE_NAME]
     e.waitUntil(
-        caches.keys().then(keyList => {
-            return keyList.filter((key) => !currentCaches.includes(key));
+        caches.keys().then(cacheNames => {
+            return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
         })
         .then((cachesToDelete) => {
             return Promise.all(
@@ -38,21 +40,42 @@ self.addEventListener("activate", function(e) {
 })
 
 self.addEventListener('fetch', function(e) {
-    if(e.request.url.startsWith(self.location.origin)) {
-        e.respondWith(
-            caches.match(e.request).then((cachedResponse) => {
-                if(cachedResponse){
-                    return cachedResponse;
-                }
+    if (
+        e.request.method !== "GET" ||
+        !e.request.url.startsWith(self.location.origin)
+    ) {
+        e.respondWith(fetch(e.request));
+        return;
+    }
 
-                return caches.open(RUNTIME).then((cache) => {
-                    return fetch(e.request).then((response) => {
-                        return cache.put(e.request, response.clone()).then(()=> {
-                            return response;
-                        })
+    if (e.request.url.includes("/api/transaction")) {
+
+        e.respondWith(
+            caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(e.request)
+                    .then(response => {
+                        cache.put(e.request, response.clone());
+                        return response;
                     })
-                })
+                    .catch(() => caches.match(e.request));
             })
         );
+        return;
     }
+
+    e.respondWith(
+        caches.match(e.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            return caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(e.request).then(response => {
+                    return cache.put(e.request, response.clone()).then(() => {
+                        return response;
+                    });
+                });
+            });
+        })
+    );
 })
